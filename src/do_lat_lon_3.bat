e35dc92d8a48d1ab4be2dad62f5d128812c2de27
Andy Cable <andycable262@gmail.com>
2022-06-30 19:17:59 -0400

    reduced resolution within all.3.uniq.csv

--- src/do_lat_lon_3.bat
+++ src/do_lat_lon_3.bat
@@ -3,16 +3,16 @@ mkdir out
 del out\all.3.txt
 for %%f in (*.gpx) do findstr lat %%f |cut --bytes 16-21,33-39,43-43 --output-delimiter=5,|uniq|sort|uniq >>out\all.3.txt
 
-sed -i -b s/05,/10,/g out\all.3.txt
-sed -i -b s/15,/10,/g out\all.3.txt
-sed -i -b s/25,/30,/g out\all.3.txt
-sed -i -b s/35,/30,/g out\all.3.txt
-sed -i -b s/45,/50,/g out\all.3.txt
-sed -i -b s/55,/50,/g out\all.3.txt
-sed -i -b s/65,/70,/g out\all.3.txt
-sed -i -b s/75,/70,/g out\all.3.txt
-sed -i -b s/85,/90,/g out\all.3.txt
-sed -i -b s/95,/90,/g out\all.3.txt
+sed -i -b s/05,/25,/g out\all.3.txt
+sed -i -b s/15,/25,/g out\all.3.txt
+sed -i -b s/25,/25,/g out\all.3.txt
+sed -i -b s/35,/25,/g out\all.3.txt
+sed -i -b s/45,/25,/g out\all.3.txt
+sed -i -b s/55,/75,/g out\all.3.txt
+sed -i -b s/65,/75,/g out\all.3.txt
+sed -i -b s/75,/75,/g out\all.3.txt
+sed -i -b s/85,/75,/g out\all.3.txt
+sed -i -b s/95,/75,/g out\all.3.txt
 
 echo lat,long,extra > C:\Repo\walk-ct\src\all.3.uniq.csv
 sort out\all.3.txt|uniq >>C:\Repo\walk-ct\src\all.3.uniq.csv
