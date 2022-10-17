f9ec5506b82762c84280d07181f53e7a6323c179
Andy Cable <andycable262@gmail.com>
2022-10-16 20:38:06 -0400

    updated lat/long csv files

--- src/do_lat_lon_2.bat
+++ src/do_lat_lon_2.bat
@@ -4,4 +4,19 @@ del out\all.2.txt
 for %%f in (*.gpx) do findstr lat %%f |cut --bytes 16-20,33-38,43-43 --output-delimiter=5,|uniq|sort|uniq >>out\all.2.txt
 echo lat,long,extra > C:\Repo\walk-ct\src\all.2.uniq.csv
 sort out\all.2.txt|uniq >>C:\Repo\walk-ct\src\all.2.uniq.csv
+
+copy out\all.2.txt out\all.25.txt
+sed -i -b s/05,/25,/g out\all.25.txt
+sed -i -b s/15,/25,/g out\all.25.txt
+sed -i -b s/25,/25,/g out\all.25.txt
+sed -i -b s/35,/25,/g out\all.25.txt
+sed -i -b s/45,/25,/g out\all.25.txt
+sed -i -b s/55,/75,/g out\all.25.txt
+sed -i -b s/65,/75,/g out\all.25.txt
+sed -i -b s/75,/75,/g out\all.25.txt
+sed -i -b s/85,/75,/g out\all.25.txt
+sed -i -b s/95,/75,/g out\all.25.txt
+echo lat,long,extra > C:\Repo\walk-ct\src\all.25.uniq.csv
+sort out\all.25.txt|uniq >>C:\Repo\walk-ct\src\all.25.uniq.csv
+
 popd
