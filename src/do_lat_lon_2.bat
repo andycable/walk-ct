1ef8bcedaf2ef7f546488ee0408841b74160836c
Andy Cable <andycable262@gmail.com>
2023-03-28 09:55:11 -0400

    created additional lat/long granularities

--- src/do_lat_lon_2.bat
+++ src/do_lat_lon_2.bat
@@ -19,4 +19,19 @@ sed -i -b s/95,/75,/g out\all.25.txt
 echo lat,long,extra > C:\Repo\walk-ct\src\all.25.uniq.csv
 sort out\all.25.txt|uniq >>C:\Repo\walk-ct\src\all.25.uniq.csv
 
+copy out\all.2.txt out\all.11.txt
+
+sed -i -b s/05,/1,/g out\all.11.txt
+sed -i -b s/15,/1,/g out\all.11.txt
+sed -i -b s/25,/3,/g out\all.11.txt
+sed -i -b s/35,/3,/g out\all.11.txt
+sed -i -b s/45,/5,/g out\all.11.txt
+sed -i -b s/55,/5,/g out\all.11.txt
+sed -i -b s/65,/7,/g out\all.11.txt
+sed -i -b s/75,/7,/g out\all.11.txt
+sed -i -b s/85,/9,/g out\all.11.txt
+sed -i -b s/95,/9,/g out\all.11.txt
+echo lat,long,extra > C:\Repo\walk-ct\src\all.11.uniq.csv
+sort out\all.11.txt|uniq >>C:\Repo\walk-ct\src\all.11.uniq.csv
+
 popd
--- src/do_lat_lon_3.bat
+++ src/do_lat_lon_3.bat
@@ -3,6 +3,22 @@ mkdir out
 del out\all.3.txt
 for %%f in (*.gpx) do findstr lat %%f |cut --bytes 16-21,33-39,43-43 --output-delimiter=5,|uniq|sort|uniq >>out\all.3.txt
 
+copy out\all.3.txt out\all.5.txt
+
+sed -i -b s/05,/1,/g out\all.5.txt
+sed -i -b s/15,/1,/g out\all.5.txt
+sed -i -b s/25,/3,/g out\all.5.txt
+sed -i -b s/35,/3,/g out\all.5.txt
+sed -i -b s/45,/5,/g out\all.5.txt
+sed -i -b s/55,/5,/g out\all.5.txt
+sed -i -b s/65,/7,/g out\all.5.txt
+sed -i -b s/75,/7,/g out\all.5.txt
+sed -i -b s/85,/9,/g out\all.5.txt
+sed -i -b s/95,/9,/g out\all.5.txt
+
+echo lat,long,extra > C:\Repo\walk-ct\src\all.5.uniq.csv
+sort out\all.5.txt|uniq >>C:\Repo\walk-ct\src\all.5.uniq.csv
+
 sed -i -b s/05,/25,/g out\all.3.txt
 sed -i -b s/15,/25,/g out\all.3.txt
 sed -i -b s/25,/25,/g out\all.3.txt
