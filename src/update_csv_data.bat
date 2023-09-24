b76062c4d57820bd79986801a770aab136a10ee0
Andrew Cable <acable@powwr.com>
2023-09-24 11:18:59 -0400

    created 4 decimal low-resolution data

--- src/update_csv_data.bat
+++ src/update_csv_data.bat
@@ -2,6 +2,7 @@ copy gpx\*.gpx \export_55533644\activities
 call do_lat_lon_1.bat
 call do_lat_lon_2.bat
 call do_lat_lon_3.bat
+call do_lat_lon_4.bat
 git add *.csv
 git commit -m "updated lat/long csv files"
 git push
