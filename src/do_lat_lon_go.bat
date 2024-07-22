94163efe36b6ad7ec40f121dc1e9ae1c4c1ee3c8
Andrew Cable <acable@powwr.com>
2024-07-21 21:47:25 -0400

    Cleaned up logic for do_lat_lon_go.bat.  Convert all.3.uniq.csv into all.2.uniq.csv

--- src/do_lat_lon_go.bat
+++ src/do_lat_lon_go.bat
@@ -1,8 +1,15 @@
 pushd C:\export_55533644\activities
 mkdir out
 del out\all.3.txt
+del out\all.2.txt
 
 C:\Repo\walk-ct\src\go_gpx2latlong\go_gpx2latlong.exe> out\all.3.txt
+cat out\all.3.txt|cut --bytes 1-5,7-14,16-18 >out\all.2.txt
+
 echo lat,long,extra > C:\Repo\walk-ct\src\all.3.uniq.csv
 sort out\all.3.txt|uniq >>C:\Repo\walk-ct\src\all.3.uniq.csv
+
+echo lat,long,extra > C:\Repo\walk-ct\src\all.2.uniq.csv
+sort out\all.2.txt|uniq >>C:\Repo\walk-ct\src\all.2.uniq.csv
+
 popd
