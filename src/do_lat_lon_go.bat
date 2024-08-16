e9de78a12e9d126e376c1971bf1ab9d70ac7eac1
Andrew Cable <acable@powwr.com>
2024-08-16 12:39:33 -0400

    Added medium resolution all.5.uniq.csv

--- src/do_lat_lon_go.bat
+++ src/do_lat_lon_go.bat
@@ -12,4 +12,19 @@ sort out\all.3.txt|uniq >>C:\Repo\walk-ct\src\all.3.uniq.csv
 echo lat,long,extra > C:\Repo\walk-ct\src\all.2.uniq.csv
 sort out\all.2.txt|uniq >>C:\Repo\walk-ct\src\all.2.uniq.csv
 
+copy out\all.3.txt out\all.5.txt
+
+sed -i -b s/05,/10,/g out\all.5.txt
+sed -i -b s/15,/10,/g out\all.5.txt
+sed -i -b s/25,/30,/g out\all.5.txt
+sed -i -b s/35,/30,/g out\all.5.txt
+sed -i -b s/45,/50,/g out\all.5.txt
+sed -i -b s/55,/50,/g out\all.5.txt
+sed -i -b s/65,/70,/g out\all.5.txt
+sed -i -b s/75,/70,/g out\all.5.txt
+sed -i -b s/85,/90,/g out\all.5.txt
+sed -i -b s/95,/90,/g out\all.5.txt
+
+echo lat,long,extra > C:\Repo\walk-ct\src\all.5.uniq.csv
+sort out\all.5.txt|uniq >>C:\Repo\walk-ct\src\all.5.uniq.csv
 popd
